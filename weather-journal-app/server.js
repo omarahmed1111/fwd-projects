975067bfb3f6cc17b8f5b6a94b427b3ae3cddcdb
Omar Ahmed <omarpirate2010@yahoo.com>
2020-10-09 05:06:10 +0200

    Add the cors as a middleware for express

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -4,6 +4,7 @@ projectData = {};
 // Require Express to run server and routes
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 // Start up an instance of app
 const app = express();
 /* Middleware*/
@@ -12,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
